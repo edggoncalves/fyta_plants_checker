ded97d8c94c8e695483b66ff76c4a6398ed9ae4f
Edgar Goncalves <edgar.gomes.goncalves@zalando.de>
2023-07-24 12:45:36 +0200

    Corrected sentences

--- immutables.py
+++ immutables.py
@@ -1,6 +1,6 @@
 STATUS = {
-    "hungry": "The following plant(s) could some nutrients:\n",
-    "thirsty": "The following plant(s) could some water:\n",
+    "hungry": "The following plant(s) could use some nutrients:\n",
+    "thirsty": "The following plant(s) could use some water:\n",
     "dark":  "The following plant(s) could use more light:\n",
     "low_battery": "The following plant(s)' sensor battery is running low:\n",
 }
